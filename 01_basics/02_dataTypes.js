edad42d95b880b0fa6c954feb8a10e12d5360ca9
ArifLucky1 <ariflucky1@gmai.com>
2025-04-25 20:15:09 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";    //treat all JS code as newer version
+
+// alert(3 + 3)        //error,  we are using node.js, not browser
+
+console.log("Arif");
+
+
+let name = "Arif"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value   this is kind of type. wo khali h. null means empty.
+// undefined => 
+// symbol => unique
+
+// object
+
+
+console.log(typeof age);
+console.log(typeof undefined);      //undefined because it is a type in js.
+console.log(typeof null);   //object

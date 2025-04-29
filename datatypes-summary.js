762d23aa2d14cd98aebee0f9cd85358cf52a28aa
ArifLucky1 <109233884+ArifLucky1@users.noreply.github.com>
2025-04-29 18:04:06 +0000

    datatypes primitive or non-primitive

---
+++ datatypes-summary.js
@@ -0,0 +1,5 @@
+// Primitive-  Call by value type h..  reference nii diya jata h original data copy kr ke diya jata h.. 
+    // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Non Primitive - Reference type.
+    // Arrays, Objects, functions

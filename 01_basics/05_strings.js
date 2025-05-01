e0ced3d82d1bcd56a792d3978cc0e61fdd5093c2
ArifLucky1 <109233884+ArifLucky1@users.noreply.github.com>
2025-05-01 15:46:35 +0000

    comparision

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,22 @@
+// let string = "Arif"     //String works with both single '' And double "" inverted commas.
+// let string = 'Arif'
+
+const name = "Arif"
+const repoCount = 50
+
+// console.log(name +  " repoCount" + " Value");       // This is old ways to concatinate strings. Avoid, this is not recommended.
+
+// Use backticks ``
+
+// console.log(`Hello My name is ${name} and my repo Count is ${repoCount}`);
+
+// another way to declared string:
+
+const gameName = new String('Mohd-Arif')        //Do gameName in Browser console.. That is not Array. string is a object key value pairs type.
+// console.log(gameName);
+// console.log(gameName[0]);       M
+// console.log(gameName[1]);       o
+// console.log(gameName[2]);       h
+// console.log(gameName[3]);       d
+// console.log(gameName[4]);       -
+// console.log(gameName[5]);       A

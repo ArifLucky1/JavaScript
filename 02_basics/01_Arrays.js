e2b808bb8b135d322f392d50a84038b61943d471
ArifLucky1 <109233884+ArifLucky1@users.noreply.github.com>
2025-05-03 16:18:20 +0000

    Array_2 and rename Array_1 file

--- 02_basics/01_Arrays.js
+++ 02_basics/01_Arrays.js
@@ -5,7 +5,7 @@
 
 // how initialize Array.
 
-const myarr = [0, 1, 2, 3, 4, 5]
+// const myarr = [0, 1, 2, 3, 4, 5]
 // const myHeros = ["Spiderman", "Ironman"] 
 // const myarr2 = new Array(1, 2, 3, 4)
 // console.log(myarr[1]);
@@ -29,16 +29,16 @@ const myarr = [0, 1, 2, 3, 4, 5]
 
 // slice, splice
 
-console.log("A ", myarr);
+// console.log("A ", myarr);
 
-const myn1 = myarr.slice(1, 3)
+// const myn1 = myarr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myarr);
+// console.log(myn1);
+// console.log("B ", myarr);
 
-const myn2 = myarr.splice(1, 3)             // splice operation orignal array ko muniplate krta h
-console.log("C ", myarr);
-console.log(myn2);
+// const myn2 = myarr.splice(1, 3)             // splice operation orignal array ko muniplate krta h
+// console.log("C ", myarr);
+// console.log(myn2);
 
 
 

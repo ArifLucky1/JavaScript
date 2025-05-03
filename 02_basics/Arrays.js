54b00ff86e4a635322a93ad929b72eb3a7813920
ArifLucky1 <109233884+ArifLucky1@users.noreply.github.com>
2025-05-03 16:08:17 +0000

    slice and splice

--- 02_basics/Arrays.js
+++ 02_basics/Arrays.js
@@ -5,7 +5,7 @@
 
 // how initialize Array.
 
-// const myarr = [0, 1, 2, 3, 4, 5]
+const myarr = [0, 1, 2, 3, 4, 5]
 // const myHeros = ["Spiderman", "Ironman"] 
 // const myarr2 = new Array(1, 2, 3, 4)
 // console.log(myarr[1]);
@@ -28,3 +28,19 @@
 
 
 // slice, splice
+
+console.log("A ", myarr);
+
+const myn1 = myarr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myarr);
+
+const myn2 = myarr.splice(1, 3)             // splice operation orignal array ko muniplate krta h
+console.log("C ", myarr);
+console.log(myn2);
+
+
+
+
+

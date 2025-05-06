d7b1f5bcd6abf84be7e835987b1e1387376ab1aa
ArifLucky1 <109233884+ArifLucky1@users.noreply.github.com>
2025-05-06 14:25:04 +0000

    JSON structure

--- 03_Objects/03_object.js
+++ 03_Objects/03_object.js
@@ -11,3 +11,16 @@ const course = {
 const {courseInstructor: instructor} = course           // Object de-structure
 // console.log(instructor);
 
+// ***********JSON*******************
+
+{
+    "name": "Arif",                                   //object ka name hota h.. sirf {} use krna matlb JSON h..
+    "coursename": "js on youtube",                    // JSON me key or values dono String me hoti h..
+    "price": "free"
+}
+
+[
+    {},                                         //may be in form of array also.. Api.
+    {},
+    {}
+]

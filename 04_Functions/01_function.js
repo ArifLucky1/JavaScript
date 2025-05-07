dff6f569c0bcdeda0e36233465fdf00c7e7a21c2
ArifLucky1 <109233884+ArifLucky1@users.noreply.github.com>
2025-05-07 18:09:06 +0000

    function with  array

--- 04_Functions/01_function.js
+++ 04_Functions/01_function.js
@@ -58,13 +58,32 @@
 
 // Object
 
-const user = {
-    username : "Arif",
-    price : 199 
-}
+// const user = {
+//     username : "Arif",
+//     price : 199 
+// }
 
-function handleObject(anyobject){
-    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
     
-}
-handleObject(user)
+// }
+// // handleObject(user)
+// // Direct object bhi paass kr skte h..
+// handleObject(
+//     {
+//         username: "sam",
+//         price: 399
+//     }
+// )
+
+// // Arrays
+
+// const myNewArray = [200, 400, 100, 600]
+// function returnSecondValue(getArray){
+//     return getArray[1]
+// }
+
+// // console.log(returnSecondValue(myNewArray));
+
+// console.log(returnSecondValue([200, 400, 500, 1000]));
+

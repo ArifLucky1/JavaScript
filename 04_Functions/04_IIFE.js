8a72cc78104b6344cb164693eb01249e4ff458c3
ArifLucky1 <109233884+ArifLucky1@users.noreply.github.com>
2025-05-09 18:07:49 +0000

    IIFE update

--- 04_Functions/04_IIFE.js
+++ 04_Functions/04_IIFE.js
@@ -8,17 +8,24 @@
 // }
 // chai()
 
-(function chai() {
-    console.log("DB CONNECTED");
+// (function chai() {
+//     console.log(`DB CONNECTED`);
     
-})
-()
+// })
+// ();
 
 // ()()    first parenthesis jahan pr function ki definition likhne wale h.. Second wala execution call ke lye..
 // IIFE envoke to ho jata h..but use pta nii rukna kaha h..isko end krna bhi zruri h..isko end krne k lye ek ; ka use krte h..
 
-(function chai aurcode() {
-    console.log("DB CONNECTED");
+// (function chaiaurcode() {
+//     console.log(`DB CONNECTED`);
+    
+// })
+// ()
+
+((name) => {
+    console.log(`DB CONNECTED Two ${name}`);
     
 })
-()
+("Arif")
+

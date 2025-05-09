788ca561aa668a35f009abe3015db240148177a1
ArifLucky1 <109233884+ArifLucky1@users.noreply.github.com>
2025-05-09 18:48:31 +0000

    for loop, break, continue

--- 06_itration_loop/01_for.js
+++ 06_itration_loop/01_for.js
@@ -12,13 +12,45 @@ for (let i = 0; i <= 10; i++) {
     
 }
 
-for (let i = 0; i <=  10; i++) {
-    console.log(`outer loop value ${i}`);
+for (let i = 1; i <=  10; i++) {
+    // console.log(`outer loop value ${i}`);
     
-    for (let j = 0; j <= 10; j++) {
-        console.log(`Inner loop value ${j} and inner loop ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner loop value ${j} and inner loop ${i}`);
+        // console.log(i + "*" + j + "=" + i*j );
         
         
     }
+    
+}
+
+let myArray = ["flash", "superman", "batman"]
+// console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {          // 0th position se start ho rha h..islye <= nii hoga.. undefined ho jyega
+    const element = myArray[index];                             // index++ hona bhut zruri h nii to flash pr hi atak jayega..
+    // console.log(element);
+    
+}
+
+// break and continue
+
+// for (let index = 0; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`detected 5`);
+//         break
+        
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+
+for (let index = 0; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`detected 5`);
+        continue
+        
+    }
+    console.log(`value of i is ${index}`);
     
 }

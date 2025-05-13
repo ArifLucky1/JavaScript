262a0a848f8db767d9c0d70fc11108ba8214cd48
ArifLucky1 <109233884+ArifLucky1@users.noreply.github.com>
2025-05-13 18:12:32 +0000

    for-in and for-of loop on object and array

--- 06_iteration_loop/03_highOrderLoop.js
+++ 06_iteration_loop/03_highOrderLoop.js
@@ -16,3 +16,65 @@ for (const greet of greetings) {
     // console.log(`Each char is ${greet}`);
     
 }
+
+
+// *********maps**********      map object hjo ki hold krta h. key value pairs..
+
+const map = new Map()
+
+map.set('IN', "India")
+map.set('USA', "United State of America")
+map.set('Fr', "France")
+map.set('IN', "India")          // no repaeted value.. uniqueness
+
+// console.log(map);
+
+for (const [key, value] of map) {            // destructuring krdeta h
+// for (const key of map) {         
+    // console.log(key, ':-', value);
+    
+}
+
+// for-of loop for object..
+
+// const myObject = {
+//     'game1' : 'NFS',
+//     'game2' : 'Spiderman'
+// }
+// for (const [key, value] of myObject) {
+//     console.log();
+//     // console.log(key, ':-', value);          //TypeError: myObject is not iterable
+// }
+
+
+
+
+// object iteration kaise krte h for-in use krke..
+
+// const myObject = {
+//     Js : 'javaScript',
+//     Cpp : 'C++',
+//     Rb : 'rubby',
+//     swift : "swift by apple"
+
+// }
+
+// for (const key in myObject) {
+// //    console.log(key);
+// //    console.log(myObject[key]);
+//    console.log(` ${key} shortcut is for ${myObject[key]}`);
+   
+// }
+
+
+
+
+// for-in for Arrays
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+  //console.log(key);            //0, 1, 2, 3, 4
+    console.log(programming[key]);
+    
+}

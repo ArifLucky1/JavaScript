2fecd7e48414239191eb9caf693ce7b0b308fc22
ArifLucky1 <109233884+ArifLucky1@users.noreply.github.com>
2025-05-13 17:44:37 +0000

    for-of loop

--- 06_iteration_loop/03_highOrderLoop.js
+++ 06_iteration_loop/03_highOrderLoop.js
@@ -7,6 +7,12 @@
 const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
-    console.log(num);
+    // console.log(num);
+    
+}
+
+const greetings ="Hello World!"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
     
 }

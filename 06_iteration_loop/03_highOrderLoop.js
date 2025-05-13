e547f0cc53ecb1401d0024fedf0eefbf69ae0488
ArifLucky1 <109233884+ArifLucky1@users.noreply.github.com>
2025-05-13 18:36:40 +0000

    object in Array  with forEach

--- 06_iteration_loop/03_highOrderLoop.js
+++ 06_iteration_loop/03_highOrderLoop.js
@@ -98,3 +98,37 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 // })
 
 
+// function printMe(item) {
+//     console.log(item);
+    
+// }
+// coding.forEach(printMe)
+
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+    
+// })
+
+
+
+// [{}, {}, {}]
+const mycoding = [
+    {
+        languageName : "javaScript",
+        languageFileName : "js"
+    },
+    {
+    languageName : "java",
+    languageFileName : "java"
+    },
+    {
+    languageName : "python",
+    languageFileName : "py"
+    },
+]
+
+mycoding.forEach ( (item) => {
+    console.log(item.languageName);
+    
+})

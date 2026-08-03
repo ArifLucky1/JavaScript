9e7bd3b048f570d25a4aac11f293d52995365486
Mohd Arif <ariflucky1@gmail.com>
2026-08-03 23:21:06 +0530

    this keyword

---
+++ Interview_Questions/14. this_keyword.js
@@ -0,0 +1,27 @@
+// "this" refers to the object that is executing the current function.
+
+const { StrictMode } = require("react")
+
+// * In method      -> Object 
+// * In function    -> global (or undefined in StrictMode)
+// * In Arrow fn    -> lexical (inherits from parent) 
+// * In constructor -> new instance
+
+
+
+    const obj = {
+        name: "Arif",
+        sayHi(){
+            console.log(this.name);
+        }
+    };
+
+    obj.sayHi();                 // Arif
+
+    const fn = obj.sayHi;
+    fn()                        // undefined in strict mode
+
+
+    const arrow = ();  
+    console.log(this);
+    arrow();                    // window (or undefined in strict mode)

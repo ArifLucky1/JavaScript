b696ce2731e711af35553550abef74c76fb40558
Mohd Arif <ariflucky1@gmail.com>
2026-08-04 23:18:45 +0530

    reduce

---
+++ Interview_Questions/23. reduce.js
@@ -0,0 +1,9 @@
+// reduce() reduces the array to a single value by accumulating results.
+
+
+    const nums = [1, 2, 3, 4]
+
+    const sum = nums.reduce((acc,cur) => acc + cur, 0);
+
+    console.log(sum);
+    

6bad81802354e02a2c323a6deb6617d7926ef187
Mohd Arif <ariflucky1@gmail.com>
2026-08-04 21:06:57 +0530

    Inheritance

---
+++ Interview_Questions/20. Inheritance.js
@@ -0,0 +1,17 @@
+// Inheritance allows a class to inherit properties and methods from another class using "extends".
+
+
+    class Animal{
+        speak(){
+            return "Animal speaks";
+        }
+    }
+
+    class Dog extends Animal{
+        speak(){
+            return "Dog barks";
+        }
+    }
+
+    const d = new Dog();
+    console.log(d.speak());                 //Dog barks

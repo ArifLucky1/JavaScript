e48e539a2d42e78197c737dffa8b8c18c8bbccac
Mohd Arif <ariflucky1@gmail.com>
2026-08-04 23:13:21 +0530

    filter

---
+++ Interview_Questions/22. filter.js
@@ -0,0 +1,9 @@
+// filter() creates a new array with all elements that pass the test (return true)
+
+
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8]
+    
+    const even = nums.filter(n => n % 2 ===0);
+
+    console.log(even);
+    

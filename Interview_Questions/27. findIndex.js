b495b60ff9c241fc0ceaaf64b01accf064b69a28
Mohd Arif <ariflucky1@gmail.com>
2026-08-05 22:59:46 +0530

    findIndex()

---
+++ Interview_Questions/27. findIndex.js
@@ -0,0 +1,9 @@
+// findIndex() returns the index of the first element that satisfies the condition.
+
+
+    const nums = [10, 20, 30, 40]
+
+    const idx = nums.findIndex(n => n === 30);
+
+    console.log(idx);               // 2
+    

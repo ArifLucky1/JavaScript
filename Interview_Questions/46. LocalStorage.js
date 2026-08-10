779fc37ca012bceed582f71cbdb82c26126c5bf7
Mohd Arif <ariflucky1@gmail.com>
2026-08-10 23:36:06 +0530

    LocalStorage.

---
+++ Interview_Questions/46. LocalStorage.js
@@ -0,0 +1,20 @@
+// localStorage store data with no ecpiration date.
+// Data persists even after browser is closed. 
+
+
+// Set 
+    localStorage.setItem("name", "Arif");
+
+// Get
+    let n = localStorage.getItem('name');
+
+// Remove 
+    localStorage.removeItem('name');
+
+// Clear
+    localStorage.clear();
+
+
+
+//      ***     Stores only string data.            ***
+//      ***     Capacity: ~5 - 10MB (per domain)    ***

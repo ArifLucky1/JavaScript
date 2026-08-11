61b51c473882e88a712d25f2e7d7829898a334d2
Mohd Arif <ariflucky1@gmail.com>
2026-08-11 21:38:11 +0530

    Templates literals.

---
+++ Interview_Questions/51. template_literals.js
@@ -0,0 +1,9 @@
+// Template literals allow embedded expressions and multi-line strings using backticks (``)
+
+        const name = "Arif";
+        const age = 25;
+        const msg = `Hi ${name}, you are ${age} years old.`;
+
+        console.log(msg);               // Hi Arif, you are 25 years old.
+        
+    //  *** Use `${}` to embed expressions. ***

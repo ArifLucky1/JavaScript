0f5514d2ac21682b23213917c652c7104385adc9
Mohd Arif <ariflucky1@gmail.com>
2026-08-12 23:07:12 +0530

    Rest Operator

--- Interview_Questions/35. Rest_operator.js
+++ Interview_Questions/35. Rest_operator.js
@@ -13,3 +13,23 @@
     console.log(others);
     
     
+//  Rest Operator (...) ?
+
+    // Rest operator collects multiple elements into a single array. Used in functions & destructuring.
+
+
+    // In Function
+        function sum(...nums){
+            return numns.reduce((a,b) => a+b, 0);
+        }
+
+        sum(1,2,3,4);       // 10
+
+
+    
+    // In Destructuring
+        const [a, b, ...rest] = [1,2,3,4,5];
+
+        a;      // 1
+        b;      // 2
+        rest;   // [3,4,5]

356137413300b67c5f8c7a6236d99deaae7e6d08
Mohd Arif <ariflucky1@gmail.com>
2026-08-12 23:01:49 +0530

    Spread Operator

--- Interview_Questions/34. Spread_operator.js
+++ Interview_Questions/34. Spread_operator.js
@@ -8,4 +8,33 @@
     const obj2 = {...obj1, c:3}             // {a:1, b:2, c:3}
 
     console.log(...arr1);                   // 1 2 3
-    
+
+
+
+// Spread Operators (...) ?
+
+    // Spread Operator expands an iterable (Array, String, Object) into individual elements.
+
+    // Array
+        const a = [1, 2];
+        const b = [...a, 3, 4];
+        // [1, 2, 3, 4]
+
+
+    //Object
+        const obj1 = {a:1};
+        const obj2 = {b:2};
+        const obj = {...obj1, ...obj2};
+        //{a:1, b:2}
+
+
+    // function Arguments
+        function sum(a,b,c){
+            return a+b+c;
+        }
+        const arr = [1,2,3];
+        sum(...arr);
+        // 6
+
+
+//  *** Opposite of spread is Rest Operator  ***

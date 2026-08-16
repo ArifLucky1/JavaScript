4b46fa64497497fa44dad4fd8c5d5a458ff05b81
Mohd Arif <ariflucky1@gmail.com>
2026-08-16 23:50:47 +0530

    NaN

---
+++ Interview_Questions/59. NaN.js
@@ -0,0 +1,15 @@
+
+//       NaN ??
+
+//      NaN stands for Not-a-number.
+//      It represents an invalid number result.
+
+
+        console.log(0/0);                   // NaN
+        console.log('abc' * 2);             // NaN
+        console.log(isNaN(NaN));            // true
+        console.log(Number.isNaN(NaN));     // true
+    
+
+//      ***  NaN is the only value that is not equal to itself.     NaN !== NaN  ***
+        

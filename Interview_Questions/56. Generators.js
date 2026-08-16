b7b16ee7a399b2ad8d354011aed95b237c667f64
Mohd Arif <ariflucky1@gmail.com>
2026-08-16 23:29:09 +0530

    generaators.

--- Interview_Questions/56. Generators.js
+++ Interview_Questions/56. Generators.js
@@ -2,4 +2,17 @@
     // Generators are special functions that can pause and resume using 'yield'. They return an iterator.
 
 
-        function 
+        function * count(){
+            yield 1;
+            yield 2;
+            yield 3;
+        }
+        const gen = count();
+
+        console.log(gen.next());                    // {value: 1, done: false}
+        console.log(gen.next());                    // {value: 2, done: false}
+        console.log(gen.next());                    // {value: 3, done: false}
+        console.log(gen.next());                    // {value: undefined, done: true}
+
+
+        ***  Helpful for lazy evaluation, infinite sequences, etc.   ***

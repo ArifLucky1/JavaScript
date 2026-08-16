3c6dc49b2f39e4b1b7d2239ccbc6efb5e7d58149
Mohd Arif <ariflucky1@gmail.com>
2026-08-16 23:21:12 +0530

    Promise.all Vs promise.allSettled

---
+++ Interview_Questions/55. PromiseallVsPromiseallSettled.js
@@ -0,0 +1,27 @@
+
+//   Promise.all     Vs      Promise.allSettled ?
+
+// Both run multiple promises in parallel but behave differently.
+
+
+//     Feature                      Promise.all                              Promise.allSettled
+
+//     Resolves with                Array of resolved values                 Array of all results(status + value/reason)
+//     Reject if                    Any promise rejects                      Never rejects
+//     use Case                     When all promise must succeed.           When you want results of all, success of fail   
+
+
+
+        // Promise.all
+            promise.all([p1,p2])
+            .then(res => console.log(res))
+            .then(err => console.log(err))
+
+
+        
+        //  Promise.allSettled
+                Promise.allSettled([p1,p2])
+                .then(res => console.log(res));
+                
+                // res = [{status: 'fullfilled', value:...},
+                //        {status: 'rejected', reason:...}]

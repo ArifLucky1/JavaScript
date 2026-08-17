b1e89b063334abfd9517cdb77a362c9787f93563
Mohd Arif <ariflucky1@gmail.com>
2026-08-17 23:33:19 +0530

    Type Coercion.

---
+++ Interview_Questions/60. Type_coercion.js
@@ -0,0 +1,9 @@
+    // Type coercion is automatic type conversion by JS engine during operation.
+
+    '5' + 2             //  '52' (string concatenation)
+    '5' - 2             //   3  (string -> number)
+    true + 1            //   2  (true -> 1)
+    false + 1           //   1  (false -> 0)
+
+
+//          *** Coercion happens in ==, arithmetic ops, +, if conditions, etc. ***  
